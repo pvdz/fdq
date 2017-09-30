f107363c4c8afcf8bf838b4cf65cb2ed35874d64
Peter van der Zee <github-public@qfox.nl>
2017-09-30 16:32:46 +0200

    Add jest support

---
+++ bin/jest.sh
@@ -0,0 +1,5 @@
+./node_modules/.bin/jest                                         \
+  --silent                                                       \
+  --roots tests/specs/**/*.spec.js, ../fdh/tests/**/*.spec.js    \
+  --setupFiles ./tests/specs/setup.js                            \
+;
